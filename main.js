622bb4b422b46e8f7c2929dc122ac96ae7155260
Rudra Prasad Nayak <148044045+rudra420-123@users.noreply.github.com>
2024-10-01 17:20:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,22 @@ function deleteLast(){
     let display=document.getElementById("input")
     display.value=display.value.slice(0,-1);
 }
-function appendChar(char){
-    const input=document.getElementById("input");
-    input.value+=char;
+
+function appendChar(char) {
+    const input = document.getElementById("input");
+    const lastChar = input.value.slice(-1);
+    if (['+', '-', '*', '/', '.', '%'].includes(lastChar) && ['+', '-', '*', '/', '.', '%'].includes(char)) {
+        return;
+    }
+    input.value += char;
 }
-function calculate(){
-    try{
-        input.value=eval(input.value);
+
+function calculate() {
+    try {
+        const input = document.getElementById("input")
+        input.value = eval(input.value)
     }
-    catch(error){
-        input.value="Error";
-    }
+    catch (error) {
+        document.getElementById("input").value = "error"
+    }
 }
